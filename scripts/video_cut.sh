d7334c2c1f5a81789f7a0eb759254fb45c92bd4e
Huan LI (李卓桓) <zixia@zixia.net>
2020-08-04 01:45:31 +0800

    add cut shell script

--- scripts/video_cut.sh
+++ scripts/video_cut.sh
@@ -2,5 +2,7 @@
 
 START_TIME=$1
 DURATION_TIME=$2
+INPUT_FILE=$3
+OUTPUT_FILE=$4
 
-ffmpeg -ss "$START_TIME" -t "$DURATION_TIME" -i zoom.mp4 -map 0 -c copy out.mp4
+ffmpeg -ss "$START_TIME" -t "$DURATION_TIME" -i "$INPUT_FILE" -map 0 -c copy "$OUTPUT_FILE"
